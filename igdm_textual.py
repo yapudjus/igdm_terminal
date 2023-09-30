5c0a35de8786ca51149f1bf3dd2c413d12e12df3
killian <killian35800@gmail.com>
2023-09-30 18:09:44 +0200

    jsp quel progres est fait, mais bon y a pire

--- igdm_textual.py
+++ igdm_textual.py
@@ -1,3 +1,4 @@
+from contextlib import ExitStack
 import random, json, datetime
 from textual import events, on, log
 from textual.app import App, ComposeResult, Widget
@@ -13,39 +14,48 @@ class interface(App):
     ]
     def compose(self) -> ComposeResult:
         lorem = 'Quod consequatur iste tempore et. Necessitatibus sunt occaecati officia sint. Eaque neque fugiat repellat. Ea incidunt et error animi a aliquid.Aliquid reprehenderit velit non ad non non rerum aperiam. Accusamus et culpa tenetur minima quas quis. Quod eveniet iusto quis saepe. Dolores minus natus sit quia modi suscipit iure similique. Voluptatem rerum sint ut optio nulla sequi illo.Perspiciatis nihil aut temporibus exercitationem minus quae excepturi. Ut nisi harum sunt hic. Et fugiat nostrum quo mollitia facilis alias dolores. Quis voluptate dolore distinctio autem temporibus.Assumenda nam quidem incidunt consequuntur a earum. Qui provident alias enim perspiciatis aperiam'.split(' ')
-        with open('_usernames.json', 'r') as file: usernames = json.load(file)
-        with Container(id='all') :
+        with open('cache/saved_usernames.json', 'r') as file: 
+            userlist = list()
+            tmp = json.load(file)
+            self.log(tmp)
+            for i in tmp :
+                userlist.append(tuple((i, tmp[i])))
+        self._bodycontainer = Container(id='all')
+        with self._bodycontainer :
             # with VerticalScroll(id='sidebar') :# docked left bar
             self._Sidebar = ListView(id='sidebar')
             status_list = ['online', 'dnd', 'offline', 'away']
             self.threads = []
             with self._Sidebar:
                 for i in range(25):
+                    usr= random.choice(userlist)
                     self.threads.append(self.Threadentry(
-                        id=f'thread-{i}',
-                        threadid=f'threadinstaid-{i}', 
+                        id=f'threadinstaid-{i}', 
+                        threadid=usr[0],
                         selected=False, 
                         hovered=False, 
-                        username=random.choice(usernames), 
+                        username=usr[1], 
                         status=random.choice(status_list), 
                         lastmsg=str(' '.join(random.choices(lorem, k=random.randint(1, len(lorem)))))
                     ))
                 for thread in self.threads:
                     for items in thread.compose(): yield items
 
-            with Vertical(id='msgthread') : # active message tab
-                self._messagebox = ListView(id="messages", disabled=False)
+            self._msgthread = Vertical(id='msgthread')
+            with self._msgthread : # active message tab
+                self._messagebox = VerticalScroll(id="messages", disabled=False)
                 with self._messagebox: # message list
                     self.msglist = []
                     for i in range(4):
                         self.msglist.append(self.MessageEntry(
                             msgid=str(i),
-                            username=random.choice(usernames),
+                            username=random.choice(userlist)[1],
                             msgcontent=str(' '.join(random.choices(lorem, k=random.randint(1, len(lorem))))),
                             msgtime=f'{str(random.randint(0,24)).zfill(2)}:{str(random.randint(0,59)).zfill(2)}:{str(random.randint(0,59)).zfill(2)}'
                         ))
                     for message in self.msglist:
                         for item in message.compose() : yield item
+                    self.log(self.app._compose_stacks)
                 yield Input(id='msginput', placeholder='➔ message to send')
                 
         yield Footer()
@@ -60,9 +70,15 @@ class interface(App):
     
     @on(Input.Submitted, '#msginput')
     def InputSubmitHandler(self) -> None:
-        self.sendMSG(value=self.query_one(selector='#msginput').value)
+        qres = self.query_one("#messages")
+        # self.log(qres)
+        # self.log(f'{self.ancestors_with_self(self=qres)}')
+        msginput = self.query_one(selector='#msginput')
+        msginput.value = 'SENDING...'
+        self.sendMSG(value=msginput.value)
+        self._messages__ancestorlist:list = self.query_one('#messages').ancestors_with_self
         self.appendMSG(len(self.msglist)-1)
-        self.query_one(selector='#msginput').value = ''
+        msginput.value = ''
     
     def sendMSG(self, value) -> None:
         self.msglist.append(self.MessageEntry(
@@ -73,23 +89,27 @@ class interface(App):
         ))
     
     def appendMSG(self, id) -> None:
-        # for item in self.msglist[id].compose(): self.mount(item, after=f'#msg-{int(id)-1}')
-        with self._messagebox :
-            self._messagebox.mount_all(self.msglist[id].testcomp())
+        # for item in self.msglist[id].compose(): self.mount(item, after=f'#msg-{int(id)-1}'
+        # with self.query_one('#messages'): 
+        # self.mount_all([Placeholder('tes')], before='#messages')
+        with ExitStack() as stack :
+            
+            for i in reversed(self._messages__ancestorlist[:-2]) : stack.enter_context(i)
+            self.mount_all(self.msglist[id].testcomp(), before='#messages')
         
     def rendertime(self) -> str:
         return str(datetime.datetime.now().strftime('%H:%M:%S'))
 
-    class MessageEntry() :
+    class MessageEntry():
         def __init__(self, msgid:str, username:str, msgtime:str, msgcontent:str):
             self.msgid = msgid
             self.msgtime = msgtime
             self.msgvalue = msgcontent
             self.username = username
         
-        def testcomp(self) -> Widget:
+        def testcomp(self) -> ComposeResult:
             res = []
-            with ListItem(classes='message', id=f'msg-{self.msgid}', disabled=False):
+            with Horizontal(classes='message', id=f'msg-{self.msgid}', disabled=False):
                 with Horizontal(classes='horizontal-1'):
                     res.append(Label(
                         id=f'message-{self.msgid}-username', 
@@ -118,7 +138,7 @@ class interface(App):
                     ))
             return res
 
-        def compose(self) -> ComposeResult :
+        def compose(self) :
             with ListItem(classes='message', id=f'msg-{self.msgid}', disabled=False):
                 with Horizontal(classes='horizontal-1'):
                     yield Label(
